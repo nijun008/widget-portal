8afcd98a00f512954dfe2768d11aca70cc0b8f53
NiJun <87179471@qq.com>
2022-03-30 18:55:05 +0800

    屏vuex，图标表单完善

---
+++ src/store/modules/screenList.ts
@@ -0,0 +1,59 @@
+interface Icon {
+  id: string,
+  title: string,
+  type: string,
+  size: number[],
+  rowfull: boolean,
+  iconType: string,
+  iconColor?: string,
+  iconTxt?: string,
+  iconUrl?: string,
+  url?: string,
+  libname?: string,
+  sort?: number
+}
+
+interface Screen {
+  sort: number,
+  name: string,
+  icon: string,
+  iconList: Icon[]
+}
+
+interface ScreenState {
+  list: Screen[]
+}
+
+const screenList = {
+  namespaced: true,
+  state: ():ScreenState => ({
+    list: []
+  }),
+  mutations: {
+    addScreen (state:ScreenState, screen:Screen) {
+      state.list.push(screen)
+    },
+    removeScreen(state:ScreenState, removeIndex: number) {
+      state.list.splice(removeIndex, 1)
+    },
+    editScreen(state:ScreenState, editIndex:number, name:string, icon:string) {
+      state.list[editIndex].name = name
+      state.list[editIndex].icon = icon
+    },
+    // sortIcon(state:ScreenState, screenIndex:number, oldIndex:number, newIndex:number) {
+    //   let target = state.list[screenIndex].iconList[oldIndex]
+    //   if (oldIndex !== newIndex) {
+        
+    //   }
+    // },
+    addIcon(state:ScreenState, screenIndex:number, icon:Icon) {
+      state.list[screenIndex].iconList.push(icon)
+    },
+    removeIcon(state:ScreenState, screenIndex:number, iconIndex:number) {
+      state.list[screenIndex].iconList.splice(iconIndex)
+    },
+    editIcon(state:ScreenState, screenIndex:number, iconIndex:number, icon:Icon) {
+      state.list[screenIndex].iconList[iconIndex] = {...icon}
+    }
+  }
+}
--- src/views/Index/components/IconFormModal.vue
+++ src/views/Index/components/IconFormModal.vue
@@ -2,20 +2,20 @@
   <n-modal :show="props.visible" >
     <div class="icon-form">
       <n-card title="添加网址" closable @close="close">
-        <n-form :model="urlForm">
-          <n-form-item label="网站地址"><n-input v-model:value="urlForm.url" /></n-form-item>
-          <n-form-item label="网站名称"><n-input v-model:value="urlForm.name" /></n-form-item>
+        <n-form :model="urlForm" :rules="rules" ref="formRef">
+          <n-form-item label="网站地址" path="url"><n-input v-model:value="urlForm.url" /></n-form-item>
+          <n-form-item label="网站名称" path="name"><n-input v-model:value="urlForm.name" /></n-form-item>
           <n-form-item label="选择图标">
             <div class="icon-radio-list flex">
               <div class="icon-radio-box txt-center">
-                <div class="icon-radio active">
-                  <div class="icon-radio-bg" :style="{ backgroundColor: '#1f80ef' }">名称</div>
+                <div class="icon-radio" :class="{ active: urlForm.iconType === 'color' }" @click="iconTypeClick('color')">
+                  <div class="icon-radio-bg" :style="{ backgroundColor: urlForm.iconColor }">{{ urlForm.iconTxt }}</div>
                 </div>
                 <div>纯色图标</div>
               </div>
 
               <div class="icon-radio-box txt-center">
-                <div class="icon-radio active">
+                <div class="icon-radio" :class="{ active: urlForm.iconType === 'img' }" @click="iconTypeClick('img')">
                   <div class="icon-radio-bg" :style="{ backgroundColor: '#9b59b6' }">名称</div>
                 </div>
                 <div>纯色图标</div>
@@ -28,19 +28,20 @@
               :show-alpha="false"
               v-model:value="urlForm.iconColor"
               :swatches="[
-                '#FFFFFF',
                 '#18A058',
+                '#33c5c5',
                 '#2080F0',
-                '#F0A020',
-                'rgba(208, 48, 80, 1)'
+                '#fbbc04',
+                '#e74c3c',
+                '#6c4cb9'
               ]"
             />
           </n-form-item>
 
           <n-form-item>
             <n-space>
-              <n-button>添加</n-button>
-              <n-button>添加并继续</n-button>
+              <n-button @click="saveClick">添加</n-button>
+              <n-button @click="saveClick(true)">添加并继续</n-button>
             </n-space>
           </n-form-item>
         </n-form>
@@ -50,14 +51,23 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps, defineEmits } from 'vue'
+import { defineProps, defineEmits, reactive, ref } from 'vue'
+import { FormInst } from 'naive-ui'
 
-const urlForm = {
+const formRef = ref<FormInst | null>(null)
+
+const urlForm = reactive({
   url: '',
   name: '',
   icon: '',
-  iconType: '',
+  iconType: 'color',
+  iconTxt: '名称',
   iconColor: '#18A058'
+})
+
+const rules = {
+  url: [{ required: true, message: '请输入URL', trigger: 'blur' }],
+  name: [{ required: true, message: '请输入名称', trigger: 'blur' }]
 }
 
 const props = defineProps<{
@@ -67,6 +77,22 @@ const props = defineProps<{
 const emits = defineEmits(['close'])
 const close = () => {
   emits('close')
+}
+
+// 图标类型切换
+const iconTypeClick = (iconType:string) => {
+  urlForm.iconType = iconType
+}
+
+const saveClick = (goOn:boolean | undefined) => {
+  (formRef.value as FormInst).validate(err => {
+    if (!err) {
+      console.log(err)
+      !goOn && close()
+    } else {
+      console.log(err)
+    }
+  })
 }
 </script>
 
